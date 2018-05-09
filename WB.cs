d320c7dbc9b2ef5179c6825582d7e4cf4a6e9299
Carl Johnsen <cjjohnsen@nbi.ku.dk>
2018-05-09 09:39:57 +0200

    Added Single Cycle MIPS and gitignore

---
+++ WB.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using SME;
+
+namespace SingleCycleMIPS
+{
+    [InitializedBus]
+    public interface MemToReg : IBus
+    {
+        bool flg { get; set; }
+    }
+
+    [InitializedBus]
+    public interface BufIn : IBus
+    {
+        uint data { get; set; }
+    }
+
+    [ClockedProcess]
+    public class WriteBuffer : SimpleProcess
+    {
+        [InputBus]
+        RegWriteAddr addrIn;
+        [InputBus]
+        MemOut dataIn;
+        [InputBus]
+        RegWrite regwriteIn;
+
+        [OutputBus]
+        WriteAddr addrOut;
+        [OutputBus]
+        WriteData dataOut;
+        [OutputBus]
+        WriteEnabled regwriteOut;
+
+        protected override void OnTick()
+        {
+            addrOut.val = addrIn.addr;
+            dataOut.data = dataIn.data;
+            regwriteOut.flg = regwriteIn.flg;
+        }
+    }
+}
